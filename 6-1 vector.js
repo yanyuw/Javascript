93fda402fe4ca4956572fa55306750160183f951
yanyuw <547274419@qq.com>
2018-02-01 17:43:59 +0800

    eloquent javascript ch6

---
+++ 6-1 vector.js
@@ -0,0 +1,29 @@
+function Vector(x,y){
+    this.x = x;
+    this.y = y;
+}
+
+Vector.prototype.plus = function(vector){
+    var newVector = new Vector;
+    newVector.x = this.x + vector.x;
+    newVector.y = this.y + vector.y;
+    return newVector;
+}
+Vector.prototype.minus = function(vector){
+    var newVector = new Vector;
+    newVector.x = this.x - vector.x;
+    newVector.y = this.y - vector.y;
+    return newVector;
+}
+Object.defineProperty(Vector.prototype,"length",{
+    get: function(){
+        return Math.sqrt(Math.pow(this.x,2) + Math.pow(this.y,2));
+    }
+})
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+// → Vector{x: 3, y: 5}
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+// → Vector{x: -1, y: -1}
+console.log(new Vector(3, 4).length);
+// → 5

cd1443c83e74186feb3bc56daeadf3fec240dcec
yanyuw <547274419@qq.com>
2018-02-26 17:19:38 +0800

    task

---
+++ task/js/module1.js
@@ -0,0 +1,3 @@
+module.exports = function(){
+    console.log("from module1");
+}
---
+++ task/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  entry: './js/module2.js',
+  output: {
+    path:
+    filename: 'bundle.js'
+  }
+};
+
